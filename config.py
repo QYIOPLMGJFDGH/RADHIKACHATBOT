7565ca8ffaf18cb498d898f460a81795edb6edc7
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-12-13 00:38:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAGLBTZ4MPrIG_6NZ7uvR5d-wVllARdBqlY")
+BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAFGz9hJHK0pyc21Z8RgJU1K-Lbvo5RPC7E")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
 SUPPORT_GRP = "TG_FRIENDSS"
