c71ba00d2214c83789e209a603228f485c3d0e73
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-12-14 05:20:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAFGz9hJHK0pyc21Z8RgJU1K-Lbvo5RPC7E")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+SUPPORT_GRP = "+OL6jdTL7JAJjYzVl"
+UPDATE_CHNL = "BABY09_WORLD"
+OWNER_USERNAME = "UTTAM470"
