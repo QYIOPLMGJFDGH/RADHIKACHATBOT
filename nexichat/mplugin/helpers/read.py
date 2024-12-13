13d1e01f692de7beb3d6ed23c2310cab5ae2e51a
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-12-13 18:14:07 +0530

    Update read.py

--- nexichat/mplugin/helpers/read.py
+++ nexichat/mplugin/helpers/read.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 
 
 START = """```
-[🐰⃟⃞⍣Rᴀᴅʜɪᴋᴀ❥](https://t.me/RADHIKA_CHAT_RROBOT) Cʟᴏɴᴇᦔ ❥```
+🐰⃟⃞⍣Rᴀᴅʜɪᴋᴀ❥ Cʟᴏɴᴇᦔ Bᴏᴛ ❥```
 ➥ ᴍᴜʟᴛɪ-ʟᴀɴɢᴜᴀɢᴇ /lang
 ➥ ᴇɴᴀʙʟᴇᴅ/ᴅɪꜱᴀʙʟᴇᴅ ʙʏ /chatbot
 ➥ ᴄʟᴏɴᴇ ʙʏ /clone
