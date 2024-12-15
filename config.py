8569e55c38782e784d3d0153ea2d815da47354fd
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-12-16 03:05:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAFBhF1jSnHqpTaQlpIx3YDfcksl_iqipFc")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
 SUPPORT_GRP = "+OL6jdTL7JAJjYzVl"
