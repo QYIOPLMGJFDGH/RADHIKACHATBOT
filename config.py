b524de6964c4f66d7b0fc321fdfdcacb46bdc792
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-12-18 15:47:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "6435225"
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "7638229482:AAEgKXeaodwz7LNpCk7JiYnLl-P35dVXF0E")
+BOT_TOKEN = getenv("BOT_TOKEN", "7561329328:AAEajZ_JIXo-yelp5EfZB8MHZB8ZOwjU0Rg")
 OPENAI = getenv("OPENAI", "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")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(getenv("OWNER_ID", "7400383704"))
